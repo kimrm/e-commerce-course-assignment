8163c18ae256ba8091341a0bbde1206d22c14491
Kim Rune Møller <kimrune@hotmail.no>
2024-02-25 10:05:42 +0100

    add simple animation

--- src/components/ShoppingBag/index.tsx
+++ src/components/ShoppingBag/index.tsx
@@ -1,13 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ShoppingBagContext from "../../contexts/ShoppingBagContext";
+import { motion, useAnimation } from "framer-motion";
 
 export default function ShoppingBag() {
   const contextValue = useContext(ShoppingBagContext);
   const shoppingBag = contextValue?.shoppingBag;
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({ opacity: 1, scale: [0.5, 1] }); // Start the animation
+  }, [shoppingBag, controls]); // Trigger animation when shoppingBag changes
 
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={controls}
+      transition={{ duration: 0.5 }}
+    >
       {shoppingBag?.items.length} items - {shoppingBag?.total}
-    </div>
+    </motion.div>
   );
 }
