4c8b76dfb45e1a30f03ba338c9c8d0dab7a5eeb3
Kim Rune Møller <kimrune@hotmail.no>
2024-03-15 20:58:26 +0100

    refactor

---
+++ src/contexts/ProductsContext.tsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+import { useState, useEffect, useMemo } from "react";
+import {
+  ProductsProviderProps,
+  ProductsContextValue,
+  Status,
+} from "../types/ContextTypes";
+import { IProduct } from "../types/ProductTypes";
+import { set } from "react-hook-form";
+
+export const ProductsContext = createContext<ProductsContextValue | null>(null);
+
+export function ProductsProvider(props: ProductsProviderProps) {
+  const { children } = props;
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [status, setStatus] = useState(Status.IDLE);
+  const contextValue: ProductsContextValue = useMemo(
+    () => ({
+      products: products,
+      state: status,
+      setProducts,
+    }),
+    [products, status]
+  );
+
+  useEffect(() => {
+    setStatus(Status.PENDING);
+    fetch("https://v2.api.noroff.dev/online-shop")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.data);
+        setStatus(Status.RESOLVED);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setStatus(Status.REJECTED);
+      });
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={contextValue}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
