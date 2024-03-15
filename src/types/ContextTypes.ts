568c8e19cdc257eb4c586063f779318f598b82e3
Kim Rune Møller <kimrune@hotmail.no>
2024-03-15 20:56:15 +0100

    groups context types in same file

---
+++ src/types/ContextTypes.ts
@@ -0,0 +1,18 @@
+import { IProduct } from "../types/ProductTypes";
+
+export interface ProductsContextValue {
+  products: IProduct[];
+  state: Status;
+  setProducts: React.Dispatch<React.SetStateAction<IProduct[]>>;
+}
+
+export enum Status {
+  IDLE,
+  PENDING,
+  RESOLVED,
+  REJECTED,
+}
+
+export interface ProductsProviderProps {
+  children: React.ReactNode;
+}
