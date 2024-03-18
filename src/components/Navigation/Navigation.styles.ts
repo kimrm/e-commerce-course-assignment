4c589967a959b60f10539a58996fec9c45083a38
Kim Rune Møller <kimrune@hotmail.no>
2024-03-19 00:37:22 +0100

    adds hamburger menu

--- src/components/Navigation/Navigation.styles.ts
+++ src/components/Navigation/Navigation.styles.ts
@@ -1,14 +1,23 @@
 import { NavLink } from "react-router-dom";
 import { styled } from "styled-components";
 
-export const NavigationContainer = styled.nav`
+export const NavigationContainer = styled.nav<{ open?: boolean }>`
   display: flex;
+  flex-direction: row;
+  position: static;
+  top: 100px;
+  background-color: #222;
+  padding: ${(props) => (props.open ? "1rem" : "0")};
+  right: 0;
+  left: 0;
   gap: 1rem;
   justify-content: center;
   align-items: center;
   color: white;
   @media (max-width: 768px) {
-    display: none;
+    position: absolute;
+    display: ${(props) => (props.open ? "flex" : "none")};
+    flex-direction: column;
   }
 `;
 
