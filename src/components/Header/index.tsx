984d8b0d649c028f0ffe7b9d66085ad89685685a
Kim Rune Møller <kimrune@hotmail.no>
2024-03-19 00:36:57 +0100

    adds hamburger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,46 @@
-import { Header, HeaderContainer, LogoLink } from "./Header.styles";
+import { useState } from "react";
+import {
+  HeaderRow,
+  HeaderContainer,
+  LogoLink,
+  MenuButton,
+  NavRight,
+} from "./Header.styles";
 import Navigation from "../Navigation";
 import ShoppingBag from "../ShoppingBag";
 
-export default function index() {
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuButtonClicked = () => setMenuOpen((prev) => !prev);
+
+  const handleLinkClicked = () => setMenuOpen(false);
+
   return (
-    <Header>
+    <HeaderRow>
       <HeaderContainer>
         <LogoLink to="/">Shop-a-lot</LogoLink>
-        <Navigation />
-        <ShoppingBag />
+        <Navigation open={menuOpen} linkClicked={handleLinkClicked} />
+        <NavRight>
+          <ShoppingBag />
+          <MenuButton onClick={handleMenuButtonClicked}>
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+              />
+            </svg>
+          </MenuButton>
+        </NavRight>
       </HeaderContainer>
-    </Header>
+    </HeaderRow>
   );
 }
