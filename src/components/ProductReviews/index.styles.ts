3cdde5e529b2a7182315a859a40fb8848e41ac6c
Kim Rune Møller <kimrune@hotmail.no>
2024-03-20 14:25:22 +0100

    adds styled component

---
+++ src/components/ProductReviews/index.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ReviewsContainer = styled.div`
+  ul {
+    list-style: none;
+    li {
+      margin-block-end: 1rem;
+      padding: 1rem;
+      background-color: #333;
+      border-radius: 5px;
+    }
+  }
+
+  h2 {
+    margin-block-start: 2rem;
+  }
+`;
