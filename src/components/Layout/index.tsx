44458e83492167358191c4a0807981f0bb9e0774
Kim Rune Møller <kimrune@hotmail.no>
2024-03-20 14:24:44 +0100

    Adds ScrollToTop component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,12 @@ import Footer from "../Footer";
 import { Main } from "./Main.styles";
 import { ProductsProvider } from "../../contexts/ProductsContext";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../ScrollToTop";
 
 export default function Layout() {
   return (
     <ProductsProvider>
+      <ScrollToTop />
       <Header />
       <Main>
         <Outlet />
