9b4a8f84f0e99f6c56bcff6c8679abef3b243067
Kim Rune Møller <kimrune@hotmail.no>
2024-03-20 15:22:49 +0100

    adds style changes

--- src/components/AddToCart/AddToCart.styles.ts
+++ src/components/AddToCart/AddToCart.styles.ts
@@ -1,20 +1,26 @@
 import { styled } from "styled-components";
 
 const AddToCartContainer = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: auto 1fr;
   align-items: center;
   gap: 0.5rem;
 `;
 
 const AddToCartButton = styled.button`
-  background-color: #111;
-  color: #ccc;
+  background-color: #24b90c;
+  color: #000;
   border: #444 solid 1px;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
+  height: 2.5rem;
+  font-size: 0.8rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  border-radius: 15px;
   cursor: pointer;
+  width: 100%;
   &:hover {
-    background-color: #222;
+    background-color: #4ae931;
   }
 `;
 
