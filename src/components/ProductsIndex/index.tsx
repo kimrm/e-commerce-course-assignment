e93391eea224bddf1a3dc13f59445d3db4453b18
Kim Rune Møller <kimrune@hotmail.no>
2024-03-22 15:06:01 +0100

    adds modal alert for item added to cart

--- src/components/ProductsIndex/index.tsx
+++ src/components/ProductsIndex/index.tsx
@@ -1,15 +1,20 @@
 import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useStore } from "../../store/store";
 import { IProduct } from "../../types/ProductTypes";
 import { IShoppingBagItem } from "../../types/ShoppingBagTypes";
 import { ProductsList } from "./ProductsIndex.styles";
-import ProductCard from "../ProductCard";
 import { ProductsContext } from "../../contexts/ProductsContext";
 import { Status } from "../../types/ContextTypes";
+import ProductCard from "../ProductCard";
+import Modal from "../Modal";
+import Image from "../Image";
 
 export default function ProductsIndex() {
   const { products, state } = useContext(ProductsContext) || {};
   const [displayLoader, setDisplayLoader] = useState<boolean>(false);
+  const [newCartItem, setNewCartItem] = useState<IShoppingBagItem | null>(null);
+  const [cartModalOpen, setCartModalOpen] = useState<boolean>(false);
 
   const addItemToBag = useStore((state) => state.addItemToBag);
 
@@ -39,19 +44,45 @@ export default function ProductsIndex() {
       productImage: product.image?.url,
     };
     addItemToBag(item);
+    setNewCartItem(item);
+    setCartModalOpen(true);
   }
   if (displayLoader) {
     return <div>Fetching our products catalogue. Please hold on...</div>;
   }
+
   return (
-    <ProductsList>
-      {products?.map((product: IProduct) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-          handleAddToCart={handleAddToCart}
-        />
-      ))}
-    </ProductsList>
+    <div>
+      {cartModalOpen && (
+        <Modal onClose={() => setCartModalOpen(false)}>
+          <h2>{newCartItem?.name}</h2>
+          <h3>Has been added to your cart</h3>
+          <Image
+            src={newCartItem?.productImage || ""}
+            alt={newCartItem?.name || ""}
+            height="200px"
+          />
+          <section>
+            <p>Amount: {newCartItem?.quantity}</p>
+            <p>Total: {newCartItem?.price}</p>
+          </section>
+          <button className="primary" onClick={() => setCartModalOpen(false)}>
+            Continue shopping
+          </button>
+          <Link to={"/checkout"} className="secondary">
+            Go to cart
+          </Link>
+        </Modal>
+      )}
+      <ProductsList>
+        {products?.map((product: IProduct) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </ProductsList>
+    </div>
   );
 }
