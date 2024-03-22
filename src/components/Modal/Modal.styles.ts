89e26e19ab55515c5df4f0cfc7180cae8f1ea0eb
Kim Rune Møller <kimrune@hotmail.no>
2024-03-22 15:05:19 +0100

    styles refactor

--- src/components/Modal/Modal.styles.ts
+++ src/components/Modal/Modal.styles.ts
@@ -2,27 +2,12 @@ import styled from "styled-components";
 
 export const BackDrop = styled.div`
   position: fixed;
+  z-index: 1;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
-`;
-
-export const Button = styled.button`
-  padding: 0.5rem 2rem;
-  border-radius: 5px;
-  border: none;
-  font-size: 1rem;
-  color: white;
-  cursor: pointer;
-  font-weight: bold;
-  &.primary {
-    background-color: #007bff;
-  }
-  &.secondary {
-    background-color: #6c757d;
-  }
 `;
 
 export const ModalContainer = styled.div`
@@ -35,9 +20,29 @@ export const ModalContainer = styled.div`
 `;
 
 export const ModalContent = styled.div`
-  background-color: white;
+  z-index: 2;
+  background-color: #222;
+  outline: 0.1px solid #fff;
+  color: white;
   padding: 2rem;
   border-radius: 5px;
-  color: black;
+  max-width: 90%;
   margin: auto;
+  button {
+    padding: 0.5rem 2rem;
+    border-radius: 5px;
+    border: none;
+    font-size: 1rem;
+    color: white;
+    cursor: pointer;
+    font-weight: bold;
+    margin-inline-end: 1rem;
+    margin-block-end: 1rem;
+    &.primary {
+      background-color: #007bff;
+    }
+    &.secondary {
+      background-color: #6c757d;
+    }
+  }
 `;
